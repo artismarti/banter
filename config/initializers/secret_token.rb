01a13b88c7f14a53ad9827a2208e840f9b45c456
Arti Mathanda <artismarti@gmail.com>
2013-05-23 14:34:50 +0100

    new chat project: banter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Banter::Application.config.secret_token = '7fc578ac50e5af85e0a0b563e0adce36fb126b899a093d99f1097979d8c519f51d671f650f6a58d6ea86a305904cd0b93e1fe3f1e36f2b7604471aa00b221687'
